d7031a6e4bac38f34376906b68f1b76071857407
itcommers <32547896e@gmail.com>
2019-12-15 18:18:01 +0300

    Add files via upload

---
+++ 8787/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items: 1,
+  	loop: true
+  });
+});
+
+$(".close").click(function(){
+
+$(".popup-compaign").addClass("hide")
+
+});
